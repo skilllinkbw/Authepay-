3cb053142ba0e3d4b1b1a079f94e50aedb1c5659
skilllinkbw <you@example.com>
2026-07-16 17:24:06 +0200

    fix: add client.ts, cloudflare config, and env

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',
+  experimental: {
+    serverActions: true,
+  },
+}
 module.exports = nextConfig
