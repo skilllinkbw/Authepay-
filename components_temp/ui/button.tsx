1827256fff8c930fbcd77ce0b23db112b38028a1
Lawrence Eson Mpofu <braincadeholdings@gmail.com>
2026-08-12 09:55:59 +0200

    Initial Authepay files

--- components_temp/ui/button.tsx
+++
@@ -1,8 +0,0 @@
-import { Slot } from "@radix-ui/react-slot"
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { ReactNode } from "react"
-
-export function Button({ className, children, ...props }: {className?: string, children: ReactNode}) {
-  return <button className={twMerge(clsx("bg-blue-600 text-white rounded-md px-4 py-2", className))} {...props}>{children}</button>
-}
--- components_temp/ui/input.tsx
+++
@@ -1,6 +0,0 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function Input({ className, ...props }: any) {
-  return <input className={twMerge(clsx("flex h-10 w-full rounded-md border-gray-300 px-3 py-2", className))} {...props} />
-}
